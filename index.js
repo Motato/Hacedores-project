269c4c7710d834d1b395b243a4e1e4dc333aa3b5
Hernan <hernanivan.montoya@gmail.com>
2022-10-13 23:26:32 -0500

    Hacedores project

---
+++ index.js
@@ -0,0 +1,15 @@
+// <script crossorigin src="https://unpkg.com/react@18/umd/react.production.min.js"></script>
+// <script crossorigin src="https://unpkg.com/react-dom@18/umd/react-dom.production.min.js"></script>
+
+ReactDOM.render(
+    /* <h1>Hacedores Program </h1> */
+    <ul>
+        <li>¿Quién es un hacedor?</li>
+        <li>Los proyectos de los hacedores</li>
+        <li>¿Quiénes somos?</li>
+        <li>Nuestros voluntarios</li>
+        <li>La fundación en medios de comunicación</li>
+        <li>Preguntas frecuentes</li>
+        <li>Contacto</li>
+    </ul>,
+document.getElementById("root"))
